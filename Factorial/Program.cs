19a3dd0e589dfc2a0b56ee11885ac24a8e9af427
User <User@DESKTOP-MKHOC4U>
2022-03-17 20:47:10 -0500

    Agregar archivos de proyecto.

---
+++ Factorial/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int casos, num, cantidadCeros, calCincos;
+
+Console.WriteLine("Inserte el número de casos");
+casos = int.Parse(Console.ReadLine());
+for (int i = 0; i < casos; i++)
+{
+    Console.WriteLine("ingrese un número");
+    num = int.Parse(Console.ReadLine());
+    cantidadCeros = 0;
+    calCincos = num / 5;
+    
+    
+   Console.WriteLine( "El factorial del número " + num + " contiene " + calcular (cantidadCeros,calCincos) + " ceros consecutivos a la derecha");
+}
+
+static int calcular (int cantidadCeros, int calCincos)
+{
+    while (calCincos > 0)
+    {
+        cantidadCeros += calCincos;
+        calCincos /= 5;
+    }
+    return cantidadCeros;
+}
+
+
+
+
+
+
+
+    
